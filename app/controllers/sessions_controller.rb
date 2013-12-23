5dcbb5f260232eda86c579096ee8c36b065a44dd
fushang318 <fushang318@gmail.com>
2013-12-23 17:15:54 +0800

    add sign_out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,13 @@ class SessionsController < Devise::SessionsController
     self.resource = warden.authenticate!(auth_options)
     set_flash_message(:notice, :signed_in) if is_navigational_format?
     sign_in(resource_name, resource)
-    ### respond_with resource, :location => after_sign_in_path_for(resource)
-    render :json => {:sign_in => 'ok', :location => after_sign_in_path_for(resource)} # 必须响应 json 否则会被 jquery 判为 error
+    render :json => {:sign_in => 'ok'} # 必须响应 json 否则会被 jquery 判为 error
+  end
+
+  def destroy
+    redirect_path = after_sign_out_path_for(resource_name)
+    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+    set_flash_message :notice, :signed_out if signed_out && is_navigational_format?
+    render :json => {:sign_out => 'ok'}
   end
 end
