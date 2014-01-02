28de61c0732adbeeed6760f04f1302680ad76935
fushang318 <fushang318@gmail.com>
2014-01-02 11:18:54 +0800

    实现 user login http api

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,16 +4,20 @@ class SessionsController < Devise::SessionsController
   end
 
   def create
-    self.resource = warden.authenticate!(auth_options)
+    self.resource = warden.authenticate!({:scope=>:user, :recall=>"sessions#sign_failure"})
     set_flash_message(:notice, :signed_in) if is_navigational_format?
     sign_in(resource_name, resource)
-    render :json => {:sign_in => 'ok'} # 必须响应 json 否则会被 jquery 判为 error
+    render :json => {:sign_in => 'success'} # 必须响应 json 否则会被 jquery 判为 error
+  end
+
+  def sign_failure
+    render :json => {:sign_in => 'failure'}, :status => 401
   end
 
   def destroy
     redirect_path = after_sign_out_path_for(resource_name)
     signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
     set_flash_message :notice, :signed_out if signed_out && is_navigational_format?
-    render :json => {:sign_out => 'ok'}
+    render :json => {:sign_out => 'success'}
   end
 end
