6b4869c10d4dfb6eecbadb427a500cf9bcdf38ad
fushang318 <fushang318@gmail.com>
2014-01-03 12:12:28 +0800

    未完成

--- app/models/knowledge_learned.rb
+++ app/models/knowledge_learned.rb
@@ -1,2 +1,30 @@
 class KnowledgeLearned
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :user_id,     :type => Integer
+  field :model_type,  :type => String
+  field :model_id,    :type => String
+  field :course,      :type => String
+
+  validates :model_id, :uniqueness => {
+    :scope => [:user_id, :model_type, :course]
+  }
+
+  def self.create_by_params(params)
+    id = case params[:model]
+    when KnowledgeSet
+      params[:model].set_id
+    when KnowledgeNode
+      params[:model].node_id
+    when KnowledgeCheckpoint
+      params[:model].checkpoint_id
+    end
+    KnowledgeLearned.create(
+      :user_id  => params[:user].id,
+      :model_id => id,
+      :model_type => params[:model].class.name,
+      :course => params[:course]
+    )
+  end
 end
