9c3e12fe224a4a8720fd98bf95dc6d5476e956eb
mykingla <kingla_pei@163.com>
2014-01-06 17:22:54 +0800

    rspec the experience value by everyday

--- spec/models/knowledge_learned_exp_spec.rb
+++ spec/models/knowledge_learned_exp_spec.rb
@@ -13,27 +13,59 @@ describe KnowledgeLearned do
     KnowledgeNetAdapter.find(id)
   end
 
-  before{
-    @course = "test1"
-    @net = net(@course)
-    @user = FactoryGirl.create :user
-  }
+  describe "每次做完练习增加的经验值" do
 
-  it{
-    # 第一次学习
-    exp_num = node(31).do_learn(@user)
-    exp_num.should == 10
-    # 第二次学习
-    exp_num = node(31).do_learn(@user)
-    exp_num.should == 5
-  }
+    before{
+      @course = "test1"
+      @net = net(@course)
+      @user = FactoryGirl.create :user
+    }
 
-  it{
-    # 第一次学习
-    exp_num = checkpoint(1).do_learn(@user)
-    exp_num.should == 10
-    # 第二次学习
-    exp_num = checkpoint(1).do_learn(@user)
-    exp_num.should == 5
-  }
+    it{
+      # 第一次学习
+      exp_num = node(31).do_learn(@user)
+      exp_num.should == 10
+      # 第二次学习
+      exp_num = node(31).do_learn(@user)
+      exp_num.should == 5
+    }
+
+    it{
+      # 第一次学习
+      exp_num = checkpoint(1).do_learn(@user)
+      exp_num.should == 10
+      # 第二次学习
+      exp_num = checkpoint(1).do_learn(@user)
+      exp_num.should == 5
+    }
+
+
+    describe "每天获取的经验值" do
+      before {
+        Timecop.travel(Time.now - 1.day) do
+          exp_num = node(9).do_learn(@user)
+        end
+
+        Timecop.travel(Time.now - 2.day) do
+          node(16).do_learn(@user)
+          node(16).do_learn(@user)
+          node(16).do_learn(@user)
+        end
+      }
+
+      it "1天前的经验值" do
+        @day = Time.now.to_date - 1.day
+        KnowledgeLearned.get_exp_by_day(@user, @day).should == 10
+      end
+
+      it "2天前的经验值" do
+        @day = Time.now.to_date - 2.day
+        KnowledgeLearned.get_exp_by_day(@user, @day).should == 20
+      end
+    end
+
+  end
+
+
+
 end
