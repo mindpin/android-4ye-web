239b48acdea9e63fcd4d2f28aa4aff9df8c727ce
fushang318 <fushang318@gmail.com>
2014-01-06 17:41:21 +0800

    增加 登陆登出的 rspec

---
+++ spec/controllers/session_controller_spec.rb
@@ -0,0 +1,37 @@
+require "spec_helper"
+
+describe SessionsController do
+  before{
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    @user = FactoryGirl.create :user
+  }
+
+  it "正确登陆" do
+    controller.current_user.should == nil
+    post :create, :user => {:login => @user.login, :password => '1234'} 
+    json = JSON::parse(response.body)
+    response.status.should == 200
+    json["name"].should == @user.name
+    json["login"].should == @user.login
+    json["email"].should == @user.email
+    json["avatar"].should == @user.normal_avatar_url
+    controller.current_user.should == @user
+  end
+
+  it "登陆密码错误" do
+    controller.current_user.should == nil
+    post :create, :user => {:login => @user.login, :password => 'abc'} 
+    json = JSON::parse(response.body)
+    response.status.should == 401
+    controller.current_user.should == nil
+  end
+
+  it "登出" do
+    sign_in @user
+    controller.current_user.should == @user
+    post :destroy
+    response.status.should == 200
+    controller.current_user.should == nil
+  end
+
+end
