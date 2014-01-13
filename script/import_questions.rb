4d9b718166a09359d48c7bc2604f93afa1cd6159
kaid <kaid@kaid.me>
2014-01-13 10:19:58 +0800

    fixed content_type in question import script

--- script/import_questions.rb
+++ script/import_questions.rb
@@ -1,7 +1,10 @@
 raw_data = YAML.load_file("script/data/javascript_questions.yaml")
 
+Question.destroy_all if Question.count > 0
+
 raw_data.each do |hash|
   question = Question.new(hash)
+  question.content_type = "markdown_format"
   question.knowledge_net_id = "javascript"
   question.save
 end
