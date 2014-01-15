97ce510d7eef67b47814854959e40bf9a213f4a8
fushang318 <fushang318@gmail.com>
2014-01-15 14:39:45 +0800

    fix aliyun config

--- config/initializers/r.rb
+++ config/initializers/r.rb
@@ -11,12 +11,13 @@ end
 project_4ye_path = File.expand_path("../../../",__FILE__)
 aliyun_access_id = `ruby #{project_4ye_path}/deploy/sh/parse_property.rb ALIYUN_ACCESS_ID`
 aliyun_access_key = `ruby #{project_4ye_path}/deploy/sh/parse_property.rb ALIYUN_ACCESS_KEY`
+aliyun_bucket     = `ruby #{project_4ye_path}/deploy/sh/parse_property.rb ALIYUN_BUCKET`
 
 CarrierWave.configure do |config|
   config.aliyun_access_id = aliyun_access_id
   config.aliyun_access_key = aliyun_access_key
   # 你需要在 Aliyum OSS 上面提前创建一个 Bucket
-  config.aliyun_bucket = "4ye-dev"
+  config.aliyun_bucket = aliyun_bucket
   # 是否使用内部连接，true - 使用 Aliyun 局域网的方式访问  false - 外部网络访问
   config.aliyun_internal = false
   # 配置存储的地区数据中心，默认: cn-hangzhou
