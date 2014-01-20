991a3bc79f56620dbf7745fb610470c57177400b
fushang318 <fushang318@gmail.com>
2014-01-20 18:12:26 +0800

    change login url

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Android4yeWeb::Application.routes.draw do
   root :to => 'index#index'
     # devise
-  devise_for :users, :path => 'api/account',
+  devise_for :users, :path => 'account',
                      :controllers => {
                        :sessions => :"sessions"
                      }
