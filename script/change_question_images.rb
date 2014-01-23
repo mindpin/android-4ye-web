7545570ac083eace73d9f4b4693432f1bc7f9928
fushang318 <fushang318@gmail.com>
2014-01-23 21:31:52 +0800

    add script

--- script/change_question_images.rb
+++ script/change_question_images.rb
@@ -1,10 +1,19 @@
 query = Question.where(:knowledge_net_id => "english")
 
 query.each do |question|
-  next if question.content.match(/node_.*\/.*\.png/).blank?
+  has_images = []
+  has_images << !question.content.match(/images\/.*\.jpg/).blank?
+
+  if !question.choices.blank?
+    question.choices.each do |choice|
+      has_images << !choice.match(/images\/.*\.jpg/).blank?
+    end
+  end
+
+  next if has_images.select{|f|f}.count == 0
 
   content = question.content.gsub(/!\[.*\]/) do |file_tag|
-    file_path = file_tag.match(/node_.*\/.*\.png/)[0]
+    file_path = file_tag.match(/images\/.*\.jpg/)[0]
 
     file_path = File.join("/download/question_images/", file_path)
 
@@ -12,7 +21,24 @@ query.each do |question|
     "![#{id.file.url}]"
   end
 
+  if !question.choices.blank?
+    choices = question.choices.map do |choice|
+      choice.gsub(/!\[.*\]/) do |file_tag|
+        file_path = file_tag.match(/images\/.*\.jpg/)[0]
+
+        file_path = File.join("/download/question_images/", file_path)
+
+        id = ImageData.create(:file => File.new(file_path))
+        "![#{id.file.url}]"
+      end
+    end
+  end
+
+
   question.content = content
+  if !question.choices.blank?
+    question.choices = choices
+  end
   question.save
   p question
 end
